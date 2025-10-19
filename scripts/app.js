82c9102ab6c23b96d1f0c4dd432f8707c95f5710
Noel <noel.prak@gmail.com>
2025-10-19 17:58:40 +0200

    Fix bugs and optimize responsive design

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,4 @@
-import {CONFIG} from './config.js'
-import {COUNTRY_CODES} from './counrty_codes.js';
+import {CONFIG} from '../config.js'
 
 const inputSearch = document.getElementById('input-search');
 const btnSearch = document.getElementById('btn-search');
@@ -14,160 +13,46 @@ const textWind = document.getElementById('text-wind');
 const textPressure = document.getElementById('text-pressure');
 const iconWeather = document.getElementById('icon-weather');
 const loadingWeather = document.getElementById('loading-weather');
-const currenWeather = document.getElementById('current-weather');
+const currentWeather = document.getElementById('current-weather');
 const forecastWeather = document.getElementById('5day-weather');
 const buttonRight = document.getElementById('button-right');
 const buttonLeft = document.getElementById('button-left');
-const btnCities = document.querySelectorAll('.btn-city')
-const forecastContainer = document.querySelector('.forecast-days')
-
+const btnCities = document.querySelectorAll('.btn-city');
+const forecastContainer = document.querySelector('.forecast-days');
 const suggestionsContainer = document.createElement("ul");
 suggestionsContainer.classList.add("absolute", "bg-white", "rounded-md", "shadow-md", "mt-1", "w-full", "z-50");
 inputSearch.parentElement.appendChild(suggestionsContainer);
-let cities = [];
+
+let cities = null;
+let COUNTRY_CODES = null;
 let debounceTimer;
 
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?';
 const weatherAPI5 = 'https://api.openweathermap.org/data/2.5/forecast?';
+const pictureAPI = 'https://api.pexels.com/v1/search?'
 const weatherKEY = CONFIG.OPENWEATHER_API_KEY
 
-async function initApp() {
-    await getWeather("Tokyo, Japan")
-    switchView('current');
-    loadingWeather.classList.add('hidden')
-    await loadCities();
-}
-
-initApp()
-
-//Switch between today or 5 day forecast weather
-function showEl(el) {
-    if (el) {
-        el.classList.remove('hidden');
-        el.setAttribute('aria-hidden', 'false');
-    }
-}
-
-function hideEL(el) {
-    el.classList.add('hidden');
-    el.setAttribute('aria-hidden', 'true');
-}
-
-function switchView(mode) {
-    if (mode === 'current') {
-        showEl(currenWeather);
-        hideEL(forecastWeather);
-    }
-    if (mode === 'forecast') {
-        showEl(forecastWeather);
-        hideEL(currenWeather);
-    }
-}
-
+//Event Listener
 buttonLeft.addEventListener('click', e => {
     const mode = e.currentTarget.dataset.mode;
     document.getElementById('view-text').innerText = 'Today';
     switchView(mode);
 });
+
 buttonRight.addEventListener("click", (e) => {
     const mode = e.currentTarget.dataset.mode;
     document.getElementById('view-text').innerText = '5-Day forecast';
     switchView(mode);
 });
 
-
-//Build Weather functionality
-function buildUrl(country) {
-    const val = (typeof country === 'string') ? country : country?.value;
-    if (!val || val.trim() === "") return 0;
-
-    const [city, countryName] = val.split(",").map(s => s.trim());
-    const countryCode = COUNTRY_CODES[countryName] || 0;
-    const query = countryCode ? `q=${city},${countryCode}` : `q=${city}`;
-    const unit = '&units=metric';
-    const key = '&appid=' + weatherKEY;
-
-    return weatherAPI5 + query + unit + key;
-}
-
-
-function getCountryNameByCode(code) {
-    return Object.keys(COUNTRY_CODES).find(
-        country => COUNTRY_CODES[country] === code) || null;
-}
-
-async function getWeather(inputName1) {
-    const url = buildUrl(inputName1);
-    if (!url) return;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error("Could not fetch weather resource");
-        const data = await response.json();
-        console.log(data)
-        displayWeather(data);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-function displayWeather(weatherData) {
-    cityImage(weatherData.city.name);
-    const forecastAll = getForecast(weatherData.list);
-    showForecast(forecastAll);
-    textCountry.innerText = weatherData.city.name;
-    textCity.innerText = getCountryNameByCode(weatherData.city.country);
-    textTemp.innerText = Math.trunc(weatherData.list[0].main.temp) + '°';
-    textWeatherinfo.innerText = weatherData.list[0].weather[0].description;
-    const todayDate = new Date((weatherData.list[0].dt) * 1000);
-    textTime.innerText = todayDate.toLocaleTimeString()
-    const getWeekday = date => ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    console.log(weatherData.list[0].dt_txt.get)
-
-    textFeel.innerText = Math.trunc(weatherData.list[0].main.feels_like) + '°';
-    textHumidity.innerText = weatherData.list[0].main.humidity + '%';
-    textWind.innerText = Math.trunc(weatherData.list[0].wind.speed) + ' km/h';
-    textPressure.innerText = weatherData.list[0].main.pressure + ' hPa';
-
-    //document.getElementById("loading-weather").classList.add("hidden");
-    //document.getElementById("current-weather").classList.remove("hidden");
-
-    iconWeather.src = getIcon(weatherData.list[0].weather[0].main)
-
-}
-
-//Fetch city background picture and return URL
-async function cityImage(cityName) {
-    const query = encodeURIComponent(cityName + " Skyline City");
-    const orientation = "landscape"
-    const url = `https://api.pexels.com/v1/search?query=${query}&orientation=${orientation}&per_page=1`;
-
-    try {
-        const response = await fetch(url, {
-            headers: {
-                Authorization: CONFIG.PEXELS_API_KEY
-            }
-        });
-
-        if (!response.ok) throw new Error("Pexels request failed");
-        const pictureData = await response.json();
-        const newImage = pictureData.photos[0].src.landscape;
-        document.body.style.setProperty('--bg-url', `url("${newImage}")`);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-//Input field city search auto complete
-async function loadCities() {
-    const res = await fetch("../all-countries-and-cities.json");
-    const data = await res.json();
-    cities = Object.entries(data).flatMap(([country, cityList]) =>
-        cityList.map(city => ({city, country}))
-    );
-    console.log("Loaded", cities.length, "cities");
-}
+btnSearch.addEventListener("click", () => getWeather(inputSearch));
+btnCities.forEach(btn => {
+    btn.addEventListener("click", e => {
+        const city = e.target.innerText + ", " + e.currentTarget.querySelector("span").innerText;
+        getWeather(city);
+    });
+});
 
 inputSearch.addEventListener("input", (e) => {
     clearTimeout(debounceTimer);
@@ -183,6 +68,127 @@ inputSearch.addEventListener("input", (e) => {
     }, 250);
 });
 
+inputSearch.addEventListener("keyup", event => {
+    if (event.key !== "Enter") return;
+    event.preventDefault();
+    getWeather(inputSearch);
+})
+
+//Switch between today or 5 day forecast weather
+function showElement(el) {
+    if (el) {
+        el.classList.remove('hidden');
+        el.setAttribute('aria-hidden', 'false');
+    }
+}
+
+function hideElement(el) {
+    el.classList.add('hidden');
+    el.setAttribute('aria-hidden', 'true');
+}
+
+function switchView(mode) {
+    if (mode === 'current') {
+        showElement(currentWeather);
+        hideElement(forecastWeather);
+    }
+    if (mode === 'forecast') {
+        showElement(forecastWeather);
+        hideElement(currentWeather);
+    }
+}
+
+//Weather API functions and helper functions
+async function loadCountryCodes() {
+    if (!COUNTRY_CODES) {
+        const res = await fetch("data/counrty-codes.json");
+        COUNTRY_CODES = await res.json();
+    }
+}
+
+async function buildUrl(country) {
+    const val = (typeof country === 'string') ? country : country?.value;
+    if (!val || val.trim() === "") return null;
+
+    const [city, countryName] = val.split(",").map(s => s.trim());
+    const countryCode = COUNTRY_CODES[countryName] || null;
+    const query = countryCode ? `q=${city},${countryCode}` : `q=${city}`;
+    return `${weatherAPI5}${query}&units=metric&appid=${weatherKEY}`
+}
+
+function getCountryNameByCode(code) {
+    return Object.keys(COUNTRY_CODES).find(
+        country => COUNTRY_CODES[country] === code) || null;
+}
+
+async function getWeather(inputName1) {
+    const url = await buildUrl(inputName1);
+    if (!url) return;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Error fetching weather data");
+        }
+        const data = await response.json();
+        displayWeather(data);
+    } catch (err) {
+        inputSearch.classList.add('bg-red-100');
+        setTimeout(() => inputSearch.classList.remove('bg-red-100'), 3000);
+        console.error(err);
+    }
+}
+
+function displayWeather(weatherData) {
+    const {list, city} = weatherData;
+    document.title = `Weather ${city.name}`
+    cityImage(weatherData.city.name);
+    const forecastAll = getForecast(list);
+    showForecast(forecastAll);
+    textCountry.innerText = city.name;
+    textCity.innerText = getCountryNameByCode(city.country);
+    textTemp.innerText = Math.trunc(list[0].main.temp) + '°';
+    textWeatherinfo.innerText = list[0].weather[0].description;
+    const todayDate = new Date((list[0].dt) * 1000);
+    textTime.innerText = todayDate.toLocaleTimeString()
+    textFeel.innerText = Math.trunc(list[0].main.feels_like) + '°';
+    textHumidity.innerText = list[0].main.humidity + '%';
+    textWind.innerText = Math.trunc(list[0].wind.speed) + ' km/h';
+    textPressure.innerText = list[0].main.pressure + ' hPa';
+    iconWeather.src = getIcon(list[0].weather[0].main);
+}
+
+//City background API functions and helper functions
+async function cityImage(cityName) {
+    const query = encodeURIComponent(cityName + " Skyline City");
+    const orientation = "landscape"
+
+    try {
+        const response = await fetch(`${pictureAPI}query=${query}&orientation=${orientation}&per_page=1`, {
+            headers: {
+                Authorization: CONFIG.PEXELS_API_KEY
+            }
+        });
+
+        if (!response.ok) throw new Error("Pexels request failed");
+        const pictureData = await response.json();
+        const newImage = pictureData.photos[0].src.landscape;
+        document.body.style.setProperty('--bg-url', `url("${newImage}")`);
+        document.getElementById('photographer-link').innerText = `${pictureData.photos[0].photographer}`;
+        document.getElementById('photographer-link').href = pictureData.photos[0].photographer_url;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function loadCities() {
+    const res = await fetch("data/all-countries-and-cities.json");
+    const data = await res.json();
+    cities = Object.keys(data).flatMap(country =>
+        data[country].map(city => ({city, country}))
+    );
+}
+
+//Show city suggestions to user
 function showSuggestions(query) {
     const matches = cities.filter(({city}) =>
         city.toLowerCase().startsWith(query)
@@ -195,7 +201,7 @@ function renderSuggestions(matches) {
     suggestionsContainer.innerHTML = "";
 
     if (matches.length === 0) {
-        suggestionsContainer.innerHTML = `<li class="p-2 text-gray-500">Keine Treffer</li>`;
+        suggestionsContainer.innerHTML = `<li class="p-2 text-gray-500">Nothing Found</li>`;
         return;
     }
     matches.forEach(({city, country}) => {
@@ -210,60 +216,41 @@ function renderSuggestions(matches) {
     });
 }
 
-btnSearch.addEventListener("click", () => getWeather(inputSearch));
-btnCities.forEach(btn => {
-    btn.addEventListener("click", e => {
-        const city = e.target.innerText + ", " + e.currentTarget.querySelector("span").innerText;
-        getWeather(city);
-    });
-});
-inputSearch.addEventListener("keyup", event => {
-    if (event.key !== "Enter") return;
-    event.preventDefault();
-    getWeather(inputSearch);
-})
-//Initial weather
-
-//Weather forecast 5 days
-//1. Wir bekommen die Daten
-//2. den ersten eintrag als ersten Tag nehmen!
-//3. den nachsten Tag herausfinden und davon den 1400 eintrag nehmen
-//4. Das ganze 5 mal insgesamt wiederholen!
+//Helper Functions
 function getForecast(forecast) {
-    const dateToday = new Date()
-    let weatherData = []
+    const dateToday = new Date();
+    let weatherData = [];
 
     for (let i = 0; i < forecast.length; i++) {
-        const datex = new Date(forecast[i].dt * 1000)
-        if (datex.getHours() === 14 || i === 0) {
+        const date = new Date(forecast[i].dt * 1000);
+        if (date.getHours() === 14 || i === 0) {
             weatherData.push({
-                day: datex.getDay() === dateToday.getDay() ? "Today" : weekdays[datex.getDay()],
-                date: months[datex.getMonth()].slice(0, 3) + " " + datex.getDate(),
+                day: date.getDay() === dateToday.getDay() ? "Today" : weekdays[date.getDay()],
+                date: months[date.getMonth()].slice(0, 3) + " " + date.getDate(),
                 temp: Math.trunc(forecast[i].main.temp),
                 wind: Math.trunc(forecast[i].wind.speed),
                 desc: forecast[i].weather[0].main,
                 icon: getIcon(forecast[i].weather[0].main)
             });
         } else {
-            continue
+            continue;
         }
         if (weatherData.length === 5) {
             break;
         }
     }
-    console.log(weatherData)
     return weatherData;
 }
 
 function showForecast(foreCastData) {
     forecastContainer.innerHTML = foreCastData.map(item => `
-    <div class="flex flex-row justify-between px-2 py-3 bg-[#E9EBED] rounded-xl">
+    <div class="flex flex-row justify-between px-2 py-3 bg-[#E9EBED] rounded-xl shadow-md">
       <div class="text-left w-1/3">
         <p>${item.day}</p>
         <p class="text-sm text-gray-500 font-light">${item.date}</p>
       </div>
-      <div class="flex flex-row items-center gap-2 w-1/3 justify-center">
-        <img src="${item.icon}" class="w-9 h-9">
+      <div class="flex flex-row items-center gap-2 w-1/3 justify-center flex-wrap">
+        <img src="${item.icon}" class="w-9 h-9" alt="Icon">
         <p class="text-sm text-gray-500 font-light">${item.desc}</p>
       </div>
       <div class="w-1/3 text-right">
@@ -274,30 +261,36 @@ function showForecast(foreCastData) {
   `).join("");
 }
 
-function getIcon(weatherDiscription) {
-    let srcpicture = ''
-    switch (weatherDiscription) {
+function getIcon(weatherDescription) {
+    let pictureSrc;
+    switch (weatherDescription) {
         case 'Clouds':
-            srcpicture = '../icons/bigones/scatterd-clouds.png';
+            pictureSrc = 'icons/bigones/scatterd-clouds.png';
             break;
         case 'Thunderstorm':
-            srcpicture = '../icons/bigones/lightning.png';
+            pictureSrc = 'icons/bigones/lightning.png';
             break;
         case 'Drizzle':
         case 'Rain':
-            srcpicture = '../icons/bigones/rainpng.png';
+            pictureSrc = 'icons/bigones/rainpng.png';
             break;
         case 'Snow':
-            srcpicture = '../icons/bigones/snow.png';
+            pictureSrc = 'icons/bigones/snow.png';
             break;
         case 'Clear':
-            srcpicture = '../icons/bigones/clear-sky.png';
+            pictureSrc = 'icons/bigones/clear-sky.png';
             break;
         default:
-            srcpicture = '../icons/bigones/scatterd-clouds.png';
+            pictureSrc = 'icons/bigones/scatterd-clouds.png';
             break;
     }
-    return srcpicture;
+    return pictureSrc;
 }
 
-
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadCountryCodes();
+    await getWeather("Tokyo, Japan")
+    await loadCities();
+    switchView('current');
+    loadingWeather.classList.add('hidden')
+});
